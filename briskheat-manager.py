c4124d8311c5158cc2f7d411307326ea9c04123a
Kevin Shi <Kevin.Shi@wdc.com>
2018-05-18 16:36:35 -0700

    initial creation of program to manage multiple briskheats at once

---
+++ briskheat-manager.py
@@ -0,0 +1,12 @@
+import Briskheat
+
+num_briskheats = 1
+
+#TODO: initialize all the briskheats store data in arrays
+
+#TODO: Run dump on all the bh and gather data, storing it in arrays
+
+def parse(s):
+    s_arr = s.split(' ')
+    #1 = time, 2 = date, 3 = zone, 4 = status, 5 = set-point, #6 = high alarm limit,
+    #7 = low alarm limit, 8 = actual temp, 9 = duty cycle, 10 = heater status
