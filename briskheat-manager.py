d0f2f787ef1496314d8d1f9670f6bc3b882c7721
Kevin Shi <Kevin.Shi@wdc.com>
2018-05-22 17:34:16 -0700

    manager changed so more robust

--- briskheat-manager.py
+++ briskheat-manager.py
@@ -42,26 +42,42 @@ def mass_read():
         rv.append(bh.read())
     return rv
         
-#TODO: Run dump on all the bh and gather data, storing it in arrays, dont forget to multiply temp by 10 so no decimals
+#Run dump on all the bh and gather data, storing it in arrays
+#WARNING: once in a while there is going to be a pair of bad data points due to the nature of time on machines not being exactly synced and the briskheat taking time to spew numbers. The strange numbers on time.sleep are so odd to counteract this effect.
 def dump_gather():
     mass_send('dump')
     while True:
         dump_vals = mass_read();
         #print for testing purposes
         print(dump_vals)
-        for i in range(dump_vals.length):
-            val = parse(dump_vals[i]) #vals is an array
-            data[i].append(val)
+        #TODO: check if dump_vals is empty
+        record = False
+        for vals in dump_vals:
+            if vals.length != 0:
+                record = true;
+        if record:
+# method to see if one of the ports wasn't responding, but this does not work due to the warning above.
+#            for i in range(dump_vals):
+#                if dump_vals[i].length == 0: #this means the others are outputting data, but one is not. 
+#                    print(port[i] + "is off/ not responding")
+            for i in range(dump_vals.length):
+                val = parse(dump_vals[i]) #vals is an array
+                data[i].append(val)
+            time.sleep(1)
+        time.sleep(4.63)
 
+#parses the dump log. Temp is multiplied by ten
 def parse(s):
     s_arr = s.split(' ')
     #1 = time, 2 = date, 3 = zone, 4 = status, 5 = set-point, #6 = high alarm limit,
     #7 = low alarm limit, 8 = actual temp, 9 = duty cycle, 10 = heater status
     #whats important: 4 - 1 status, 8 - 1 actual temp,
+    if s_arr.length != 10: #bad data, return 0, attempts to filter out bad data.
+        return [0]
     error_code = s_arr[3][-4:] #trims the string down to it's last 4 characters
     if (error_code != '8001'): #error has happened
         error_msg = error_code + ': ' + error_ref[error_code]
         #TODO: not sure what to do with the error, store it for now
         raised_errors.append(s_arr)
-    temp = s_arr[7]
+    temp = int(s_arr[7].float * 10)
     return [temp] #can change how much information you want to return(length of array).
