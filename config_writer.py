a6f48138eb2d03ce0b1b87b65b97550e170d7fa8
Kevin Shi <Kevin.Shi@wdc.com>
2018-07-10 13:49:47 -0700

    make ease of use config_writer

--- config_writer.py
+++ config_writer.py
@@ -1,4 +1,27 @@
 # @author Kevin Shi
+print('Welcome to the config writer')
+print('Enter the following parameters and a config.py file will be outputted, which can then be run to start the program')
+print('Port:')
+port = "'" + input() + "', "
+print('Poll Interval (x10 sec): ')
+p_interval = input() + ", "
+print('Send Interval (xPoll Interval): ')
+s_interval = input() + ", "
+print('Status Directory: ')
+status_dir = "'" + input() + "', "
+print('SQL Hostname: ')
+host = "'" + input() + "', "
+print('User: ')
+user = "'" + input() + "', "
+print('Database: ')
+db = "'" + input() + "', "
+print('Temperature Table: ')
+t = "'" + input() + "', "
+print('Logging Table: ')
+lt = "'" + input() + "'"
+
+with open('config.py', 'w') as f:
+    print("""# @author Kevin Shi
 
 import briskheat_serial_reader as bsr
 from getpass import getpass
@@ -12,7 +35,7 @@ password = ''
 #change this for Briskheat params
 def config_briskheat():
     global bh
-    bh = bsr.Briskheat('/dev/ttyUSB0', 30, 20, 'status.CSV', 'devamdatadump', 'intern_K', password, 'heatandlogs', 'briskheat_hht01', 'status_briskheat_hht01')
+    bh = bsr.Briskheat("""+port+p_interval+s_interval+status_dir+host+user+db+t+lt+""")
 
 #gets password
 def password_input():
@@ -26,4 +49,5 @@ def password_input():
         password_input()
 
 password_input()
-bh.save_dump()
+bh.save_dump()""", file = f)
+
