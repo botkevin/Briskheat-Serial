ebe6982dfa4f9bd80649d98c3bdf732f4b0d41ab
Kevin Shi <Kevin.Shi@wdc.com>
2018-07-10 13:13:33 -0700

    Make better password management

--- config.py
+++ config.py
@@ -2,13 +2,28 @@
 
 import briskheat_serial_reader as bsr
 from getpass import getpass
+import mysql.connector
 
 #runs the briskheat, also an example of usage
+
+bh = None
+password = ''
+
+#change this for Briskheat params
+def config_briskheat():
+    global bh
+    bh = bsr.Briskheat('/dev/ttyUSB0', 30, 20, 'status.CSV', 'devamdatadump', 'intern_K', password, 'heatandlogs', 'briskheat_hht01', 'status_briskheat_hht01')
+
 #gets password
-print('Input your password:')
-password = getpass()
-print(password)
+def password_input():
+    global password
+    try:
+        print('Input your password')
+        password = getpass()
+        config_briskheat()
+    except mysql.connector.errors.ProgrammingError:
+        print('Wrong password')
+        password_input()
 
-bh = bsr.Briskheat('/dev/ttyUSB0', 30, 20, 'status.CSV', 'devamdatadump', 'intern_K', password, 'heatandlogs', 'briskheat_hht01', 'status_briskheat_hht01')
-
+password_input()
 bh.save_dump()
